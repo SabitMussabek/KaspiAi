0fccc303937eccc1a3455b2aad27f90e7cd922ea
sabit <sabit.mussabek@kaspi.kz>
2024-08-08 17:52:21 +0500

    task: provide single retrofit instance

--- app/src/main/java/kz/kaspi/ai/di/AppModule.kt
+++ app/src/main/java/kz/kaspi/ai/di/AppModule.kt
@@ -1,26 +1,51 @@
 package kz.kaspi.ai.di
 
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 
 
 val appModule = module {
-//    singleOf()
-//    val jsonFormat = Json {
-//        ignoreUnknownKeys = true
-//        isLenient = true
-//        encodeDefaults = true
-//        prettyPrint = false
-//        explicitNulls = false
-//    }
-//    val contentType = "application/json".toMediaType()
-//    val retrofit = Retrofit.Builder()
-//        .baseUrl(url)
-//        .client(okHttpClient)
-//        .addConverterFactory(jsonFormat.asConverterFactory(contentType))
-//        .build()
-//
-//    return retrofit.create(T::class.java)
+    singleOf(::provideJsonFormant)
+    singleOf(::provideOkHttpClient)
+    singleOf(::provideRetrofit)
+}
+
+private fun provideRetrofit(
+    okHttpClient: OkHttpClient,
+    jsonFormat: Json,
+): Retrofit {
+    val contentType = "application/json".toMediaType()
+    return Retrofit.Builder()
+        .baseUrl("") // todo
+        .client(okHttpClient)
+        .addConverterFactory(jsonFormat.asConverterFactory(contentType))
+        .build()
+}
+
+private fun provideJsonFormant(): Json {
+    return Json {
+        ignoreUnknownKeys = true
+        isLenient = true
+        encodeDefaults = true
+        prettyPrint = false
+        explicitNulls = false
+    }
+}
+
+private fun provideOkHttpClient(): OkHttpClient =
+    OkHttpClient.Builder()
+        .addInterceptor(provideHttpLoggingInterceptor())
+        .build()
+
+
+private fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+    val interceptor = HttpLoggingInterceptor()
+    interceptor.level = HttpLoggingInterceptor.Level.BODY
+    return interceptor
 }
